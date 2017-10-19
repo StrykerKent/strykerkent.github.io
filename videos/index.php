5e615a5c8fa2a20efa18807776d950958558119d
Stryker Stinnette <strykerkent@gmail.com>
2017-10-18 23:46:32 -0500

    removed videos folder and videos page

--- videos/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head></head>
-<body>
-<h1>Wake Surf Videos 2017</h1>
-<a href="/2017/wakesurf/wakesurf_july2_2017.zip">wakesurf_july2_2017.zip</a><br /><br />
-<a href="/2017/wakesurf/wakesurf_july3_2017.zip">wakesurf_july3_2017.zip</a>
-</body>
-</html>
