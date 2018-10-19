4a6c8a1cf316fe28a434b6584996a09783779465
Stryker Stinnette <strykerkent@gmail.com>
2018-10-18 19:12:44 -0500

    add track_xref web app, added linkedin card to about, changed s logo css animation, changed contact to get in touch, added image modal for xref

---
+++ js/pagejs.js
@@ -0,0 +1,20 @@
+$(function() { //document.ready shorthand
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$('a.page-scroll').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: ($($anchor.attr('href')).offset().top - 0)
+			}, 1250, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	//make nav go away after click on mobile
+	$(document).on('click','.navbar-collapse.in',function(e) {
+	    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+	        $(this).collapse('hide');
+	    }
+	});
+	//send image src to modal... dynamically load image in modal
+    $("img").click(function() {
+      $("#modalImage").attr("src", $(this).attr("src"));
+    });
+});
